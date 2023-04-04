df4b209fad7d83b984815da39fa6cfc0ef330166
Megan Folsom <mfolsom@gmail.com>
2023-04-03 19:08:14 -0700

    Add basic lighting

--- newLevelAndSave.py
+++ newLevelAndSave.py
@@ -1,16 +1,38 @@
 import unreal
 
+def spawn_actor(actor_class, actor_name, location, rotation):
+    return unreal.EditorLevelLibrary.spawn_actor_from_class(actor_class, location, rotation)
+
 editorLevelLibrary = unreal.EditorLevelLibrary
 levelSubSys = unreal.get_editor_subsystem(unreal.LevelEditorSubsystem)
 
 # Initialize new level
-newLevel = "mainLevel"
+newMap = "mainMap"
 
 # Create new level
-mainLevel = levelSubSys.new_level("/Game/Maps/mainLevel.umap")
+mainMap = levelSubSys.new_level("/Game/Maps/" + newMap + ".umap")
+
 
 # Set level as the current level
-levelSubSys.set_current_level_by_name(newLevel)
+levelSubSys.set_current_level_by_name(newMap)
+
+# Create a SkyLight
+sky_light = spawn_actor(unreal.SkyLight.static_class(), "SkyLight", unreal.Vector(0, 0, 0), unreal.Rotator(0, 0, 0))
+
+# Create a DirectionalLight
+directional_light = spawn_actor(unreal.DirectionalLight.static_class(), "DirectionalLight", unreal.Vector(0, 0, 300), unreal.Rotator(-45, 45, 0))
+
+# Create a SkyAtmosphere
+sky_atmosphere = spawn_actor(unreal.SkyAtmosphere.static_class(), "SkyAtmosphere", unreal.Vector(0, 0, 0), unreal.Rotator(0, 0, 0))
+
+
+# Create an ExponentialHeightFog
+fog = spawn_actor(unreal.ExponentialHeightFog.static_class(), "ExponentialHeightFog", unreal.Vector(0, 0, 0), unreal.Rotator(0, 0, 0))
+
+# Create a PostProcessVolume
+post_process_volume = spawn_actor(unreal.PostProcessVolume.static_class(), "PostProcessVolume", unreal.Vector(0, 0, 0), unreal.Rotator(0, 0, 0))
+
 
 # Save level
 levelSubSys.save_current_level()
+
